3a4db1df3dd5f3ce7055d2188081de2eeed3f75b
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-03 18:15:53 +0400

    initxpo

---
+++ CS/XPO/TreeListDemo/TreeListDemo.Module/BusinessObjects/Project.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using DevExpress.Persistent.Base.General;
+using DevExpress.Xpo;
+
+namespace XAFTreeList.Module.BusinessObjects {
+    public class Project : Category {
+        private ProjectGroup projectGroup;
+        protected override ITreeNode Parent {
+            get { return projectGroup; }
+        }
+        protected override IBindingList Children {
+            get { return ProjectAreas; }
+        }
+        public Project(Session session) : base(session) { }
+        public Project(Session session, string name) : base(session) {
+            this.Name = name;
+        }
+        [Association("ProjectGroup-Projects")]
+        public ProjectGroup ProjectGroup {
+            get { return projectGroup; }
+            set { SetPropertyValue("ProjectGroup", ref projectGroup, value); }
+        }
+        [Association("Project-ProjectAreas"), Aggregated]
+        public XPCollection<ProjectArea> ProjectAreas {
+            get { return GetCollection<ProjectArea>("ProjectAreas"); }
+        }
+    }
+}
