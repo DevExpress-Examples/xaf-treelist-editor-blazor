34ef5255cd98d894d4f48360943adbebe11babfe
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-03 18:21:30 +0400

    rem

--- XPO/XAFTreeList.Blazor.Server/Services/XpoDataStoreProviderAccessor.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using DevExpress.ExpressApp.Xpo;
-
-namespace XAFTreeList.Blazor.Server.Services {
-    public class XpoDataStoreProviderAccessor {
-        public IXpoDataStoreProvider DataStoreProvider { get; set; }
-    }
-}
